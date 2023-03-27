9bfb6b8b9ad80f980614bf98cfa7b4b421283cc5
中田瑞希 <122pg0013@tokyo.amg.ac.jp>
2023-03-27 13:39:44 +0900

    Log出力を行わないようにする

--- AppFrame/source/Application/WinMain.cpp
+++ AppFrame/source/Application/WinMain.cpp
@@ -15,6 +15,8 @@
 //
 int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,_In_ LPSTR lpCmdLine,_In_ int nShowCmd)
 {
+	// LogèoóÕÇçsÇÌÇ»Ç¢
+	SetOutApplicationLogValidFlag(FALSE);
 	auto appBase = ApplicationBase::GetInstance();
 	if(!appBase)
 	{
